976c6e9c7b3f4493335fc86403dbc8c0aa28e02b
hiroki <nagadon0424@icloud.com>
2022-04-25 15:55:06 +0900

    update System  開始~終了

--- SamuraiUnity/Assets/Scripts/Player.cs
+++ SamuraiUnity/Assets/Scripts/Player.cs
@@ -8,41 +8,23 @@ using Debug = UnityEngine.Debug;
 public class Player : MonoBehaviour
 {
     [SerializeField]private SystemScript _systemScript;
-    private TimeSpan enemyTime = new TimeSpan(0, 0, 0, 3, 0);
+    
     private double PlayerTime = 0;
     private TimeSpan timer;
-    private Stopwatch time;
-    private bool boolian = false;
+  
+    
     
     
     // Start is called before the first frame update
     void Start()
     {
         //StartCoroutine("WaitTimer");
-        time = new System.Diagnostics.Stopwatch();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 a = new Vector3(1,1,1);
-        
-
-        if(Input.GetKeyDown(KeyCode.A) && _systemScript.mode == 0)
-        {
-            time.Start();
-            boolian = true;
-            Debug.Log("ON");
-        }
-
-        if (Input.GetKeyDown(KeyCode.E) && boolian)
-        {
-            time.Stop();
-            timer = time.Elapsed;
-            Debug.Log(timer);
-            time.Reset();
-            boolian = false;
-        }
     }
 
     public IEnumerable WaitTimer(float random)
@@ -50,8 +32,15 @@ public class Player : MonoBehaviour
         yield return new WaitForSeconds(random);
     }
 
-    public bool Compare(TimeSpan t1, TimeSpan t2)
-    {
-        return t1 > t2;
+    
+
+    public TimeSpan StopTime(){
+    
+        _systemScript.systemTime.Stop();
+        timer = _systemScript.systemTime.Elapsed;
+        Debug.Log(timer);
+        _systemScript.systemTime.Reset();
+        _systemScript.mode = 4;
+        return timer;
     }
 }
--- SamuraiUnity/Assets/Scripts/SystemScript.cs
+++ SamuraiUnity/Assets/Scripts/SystemScript.cs
@@ -1,15 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class SystemScript : MonoBehaviour
 {
+    [SerializeField] private Player player;
+    private TimeSpan enemyTime = new TimeSpan(0, 0, 0, 3, 0);
     public int mode; 
+    public Stopwatch systemTime;
+    private TimeSpan returnTime;
+    private bool winner;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        systemTime = new System.Diagnostics.Stopwatch();
     }
 
     // Update is called once per frame
@@ -19,5 +27,38 @@ public class SystemScript : MonoBehaviour
         {
             mode = 1;
         }
+
+        switch (mode)
+        {
+            //case 0:   メニュー画面
+            //case 1:   待機など
+            case 2:
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    Debug.Log("ON");
+                    systemTime.Start();
+                    mode = 3;
+                }
+                break;
+            case 3:
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    returnTime = player.StopTime();
+                    winner = Compare(returnTime, enemyTime);
+                }
+                break;
+            case 4:
+                if (winner) { Debug.Log("win"); }
+                else{Debug.Log("Lose");}
+                mode = 5;
+                break;
+            case 5:
+                break;
+        }
+    }
+    
+    public bool Compare(TimeSpan t1, TimeSpan t2)
+    {
+        return t1 < t2;
     }
 }
